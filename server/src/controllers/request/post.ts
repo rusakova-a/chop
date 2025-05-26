a83dde82344897622ca537f6a0170c692e20d76d
rusakova-a <rusakov.sobaka@yandex.ru>
2025-05-26 19:30:21 +0300

    Fix validation error

--- server/src/controllers/request/post.ts
+++ server/src/controllers/request/post.ts
@@ -1,13 +1,12 @@
 import { RequestRepository } from "./../../database/repositories";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
-const nameRegExp = /^[A-Z0-9\s]+$/i;
 const phoneRegExp = /^[\+\-0-9\s]+$/;
 
 export async function postRequest( req: Request, res: Response ) {
     const { phone, name } = req.body;
 
-    if (nameRegExp.test(name) && phoneRegExp.test(phone)) {
+    if (phoneRegExp.test(phone)) {
         const request = await RequestRepository.create({
             phone, name
         })
